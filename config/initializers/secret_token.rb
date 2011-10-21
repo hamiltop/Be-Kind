1911e7c8d4aaff6a3b6aaa381fc56ddff4a7e001
Peter Hamilton <peterghamilton@gmail.com>
2011-10-21 09:59:24 -0600

    empty rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Bekind::Application.config.secret_token = 'd529b43584a679c8dc4f6688eb4014b0c1370e90e5fa34611bde64a3ce21587678bdb6059acbe4a84517e84f7a053ce3c3b6335af8be4aec9891effe12e0ffc1'
